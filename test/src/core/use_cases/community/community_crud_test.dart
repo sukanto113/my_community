def14c85f360708d7407b309b2f0486495170cc8
sukanto113 <60124233+sukanto113@users.noreply.github.com>
2023-08-28 00:37:01 +0600

    restructure test

--- test/src/core/use_cases/community/community_crud_test.dart
+++ test/src/core/use_cases/community/community_crud_test.dart
@@ -133,321 +133,316 @@ void setupUserWithoutMember() {
 }
 
 void main() {
-  group('Community create with auth', () {
-    setUp(() async {
+  group('Community create', () {
+    setUp(() {
       setupSut();
-      setupAuthWithAUser();
     });
+    group('with auth', () {
+      setUp(() async {
+        setupAuthWithAUser();
+      });
 
-    test('should create a community -B', () async {
-      await sut.create(aCommunityName);
+      test('should create a community with the name -B.A', () async {
+        await sut.create(aCommunityName);
 
-      verify(communityRepo.create(any)).called(1);
-    });
-
-    test('should create a community with the name -B.A', () async {
-      await sut.create(aCommunityName);
-
-      verify(
-        communityRepo.create(
-          argThat(
-            isA<CommunityCreateDTO>()
-                .having((p0) => p0.name, "name", aCommunityName),
+        verify(
+          communityRepo.create(
+            argThat(
+              isA<CommunityCreateDTO>()
+                  .having((p0) => p0.name, "name", aCommunityName),
+            ),
           ),
-        ),
-      ).called(1);
-    });
+        ).called(1);
+      });
 
-    test('should add a member -A', () async {
-      await sut.create(aCommunityName);
+      test('should add exactly one member -A.A', () async {
+        await sut.create(aCommunityName);
 
-      verify(memberRepo.addMember(any));
-    });
+        verify(memberRepo.addMember(any)).called(1);
+      });
 
-    test('should add exactly one member -A.A', () async {
-      await sut.create(aCommunityName);
+      test('should add user as member -A.B', () async {
+        await sut.create(aCommunityName);
 
-      verify(memberRepo.addMember(any)).called(1);
-    });
-
-    test('should add user as member -A.B', () async {
-      await sut.create(aCommunityName);
-
-      verify(
-        memberRepo.addMember(
-          argThat(
-            isA<MemberCreateDto>()
-                .having((p0) => p0.userId, "userId", aUser.id),
+        verify(
+          memberRepo.addMember(
+            argThat(
+              isA<MemberCreateDto>()
+                  .having((p0) => p0.userId, "userId", aUser.id),
+            ),
           ),
-        ),
-      );
-    });
+        );
+      });
 
-    test('should add member with name same as user name -A.C', () async {
-      await sut.create(aCommunityName);
+      test('should add member with name same as user name -A.C', () async {
+        await sut.create(aCommunityName);
 
-      verify(
-        memberRepo.addMember(
-          argThat(isA<MemberCreateDto>()
-              .having((p0) => p0.name, "name", aUser.name)),
-        ),
-      );
-    });
-
-    test('should add user as admin to the community -A.D', () async {
-      await sut.create(aCommunityName);
-
-      verify(
-        memberRepo.addMember(
-          argThat(
-            isA<MemberCreateDto>().having((e) => e.role, "role", "admin"),
+        verify(
+          memberRepo.addMember(
+            argThat(isA<MemberCreateDto>()
+                .having((p0) => p0.name, "name", aUser.name)),
           ),
-        ),
-      ).called(1);
+        );
+      });
+
+      test('should add user as admin to the community -A.D', () async {
+        await sut.create(aCommunityName);
+
+        verify(
+          memberRepo.addMember(
+            argThat(
+              isA<MemberCreateDto>().having((e) => e.role, "role", "admin"),
+            ),
+          ),
+        ).called(1);
+      });
+
+      test('should add a member to the community -(A&B).A', () async {
+        when(communityRepo.create(any))
+            .thenAnswer((realInvocation) async => aCommunityId);
+
+        await sut.create(aCommunityName);
+
+        verify(
+          memberRepo.addMember(
+            argThat(isA<MemberCreateDto>()
+                .having((p0) => p0.communityId, "communityId", aCommunityId)),
+          ),
+        );
+      });
     });
-
-    test('should add a member to the community -(A&B).A', () async {
-      when(communityRepo.create(any))
-          .thenAnswer((realInvocation) async => aCommunityId);
-
-      await sut.create(aCommunityName);
-
-      verify(
-        memberRepo.addMember(
-          argThat(isA<MemberCreateDto>()
-              .having((p0) => p0.communityId, "communityId", aCommunityId)),
-        ),
-      );
+    group('without auth', () {
+      setUp(() {
+        setupAuthWithoutAUser();
+      });
+      test('should throw UserNotFound error', () {
+        expect(
+          () async {
+            await sut.create(aCommunityName);
+          },
+          throwsA(isA<UserNotFoundError>()),
+        );
+      });
     });
   });
 
-  group('Community create without auth', () {
+  group('Community update', () {
     setUp(() {
       setupSut();
-      setupAuthWithoutAUser();
     });
-    test('should throw UserNotFound error', () {
-      expect(
-        () async {
-          await sut.create(aCommunityName);
-        },
-        throwsA(isA<UserNotFoundError>()),
-      );
-    });
-  });
-
-  group('Community update with auth', () {
-    setUp(() {
-      setupSut();
-      setupAuthWithAUser();
-    });
-
-    test('should update community -C', () async {
-      setupUserWithAdminRole();
-
-      await sut.update(aCommunity);
-
-      verify(communityRepo.update(any));
-    });
-
-    test('should update exactly one time -C.A', () async {
-      setupUserWithAdminRole();
-
-      await sut.update(aCommunity);
-
-      verify(communityRepo.update(any)).called(1);
-    });
-
-    test('should update a community with same id -C.B', () async {
-      setupUserWithAdminRole();
-
-      await sut.update(aCommunity);
-
-      verify(
-        communityRepo.update(
-          argThat(
-            isA<CommunityUpdateDTO>().having((p0) => p0.id, "id", aCommunityId),
-          ),
-        ),
-      );
-    });
-
-    test('should update community name -C.D', () async {
-      setupUserWithAdminRole();
-
-      await sut.update(aCommunity);
-
-      verify(
-        communityRepo.update(
-          argThat(
-            isA<CommunityUpdateDTO>()
-                .having((p0) => p0.name, "name", aCommunityName),
-          ),
-        ),
-      );
-    });
-    test('should update community description -C.E', () async {
-      setupUserWithAdminRole();
-
-      await sut.update(aCommunity);
-
-      verify(
-        communityRepo.update(
-          argThat(
-            isA<CommunityUpdateDTO>().having(
-                (p0) => p0.description, "description", aCommunityDescription),
-          ),
-        ),
-      );
-    });
-
-    test('should update community profileImage -C.F', () async {
-      setupUserWithAdminRole();
-
-      await sut.update(aCommunity);
-
-      verify(
-        communityRepo.update(
-          argThat(
-            isA<CommunityUpdateDTO>().having((p0) => p0.profileImage,
-                "profileImage", aCommunityProfileImage),
-          ),
-        ),
-      );
-    });
-
-    test('should update community coverImage -C.G', () async {
-      setupUserWithAdminRole();
-
-      await sut.update(aCommunity);
-
-      verify(
-        communityRepo.update(
-          argThat(
-            isA<CommunityUpdateDTO>().having(
-                (p0) => p0.coverImage, "coverImage", aCommunityCoverImage),
-          ),
-        ),
-      );
-    });
-    test('should not update if user is in member role', () async {
-      setupUserWithMemberRole();
-
-      try {
-        await sut.update(aCommunity);
-      } catch (e) {}
-
-      verifyNever(communityRepo.update(any));
-    });
-  });
-
-  group('Community update without auth', () {
-    setUp(() {
-      setupSut();
-      setupAuthWithoutAUser();
-    });
-
-    test('should throw UserNotFound error', () {
-      expect(
-        () async {
+    group('with auth', () {
+      setUp(() {
+        setupAuthWithAUser();
+      });
+      group('user with admin role', () {
+        setUp(() {
+          setupUserWithAdminRole();
+        });
+        test('should call repo.update exactly once -C.A', () async {
           await sut.update(aCommunity);
-        },
-        throwsA(isA<UserNotFoundError>()),
-      );
+
+          verify(communityRepo.update(any)).called(1);
+        });
+
+        test('should update the community -C.B', () async {
+          await sut.update(aCommunity);
+
+          verify(
+            communityRepo.update(
+              argThat(
+                isA<CommunityUpdateDTO>()
+                    .having((p0) => p0.id, "id", aCommunityId),
+              ),
+            ),
+          );
+        });
+
+        group('should update property', () {
+          test('name -C.D', () async {
+            await sut.update(aCommunity);
+
+            verify(
+              communityRepo.update(
+                argThat(
+                  isA<CommunityUpdateDTO>()
+                      .having((p0) => p0.name, "name", aCommunityName),
+                ),
+              ),
+            );
+          });
+          test('description -C.E', () async {
+            await sut.update(aCommunity);
+
+            verify(
+              communityRepo.update(
+                argThat(
+                  isA<CommunityUpdateDTO>().having((p0) => p0.description,
+                      "description", aCommunityDescription),
+                ),
+              ),
+            );
+          });
+
+          test('profileImage -C.F', () async {
+            await sut.update(aCommunity);
+
+            verify(
+              communityRepo.update(
+                argThat(
+                  isA<CommunityUpdateDTO>().having((p0) => p0.profileImage,
+                      "profileImage", aCommunityProfileImage),
+                ),
+              ),
+            );
+          });
+
+          test('coverImage -C.G', () async {
+            await sut.update(aCommunity);
+
+            verify(
+              communityRepo.update(
+                argThat(
+                  isA<CommunityUpdateDTO>().having((p0) => p0.coverImage,
+                      "coverImage", aCommunityCoverImage),
+                ),
+              ),
+            );
+          });
+        });
+      });
+      group('user with member role', () {
+        setUp(() {
+          setupUserWithMemberRole();
+        });
+        test('should not call repo.update', () async {
+          try {
+            await sut.update(aCommunity);
+          } catch (e) {}
+
+          verifyNever(communityRepo.update(any));
+        });
+
+        test('should throw UserNotPermitError', () {
+          expect(
+            () async {
+              await sut.update(aCommunity);
+            },
+            throwsA(isA<UserNotPermitError>()),
+          );
+        });
+      });
+    });
+
+    group('without auth', () {
+      setUp(() {
+        setupAuthWithoutAUser();
+      });
+
+      test('should throw UserNotFound error', () {
+        expect(
+          () async {
+            await sut.update(aCommunity);
+          },
+          throwsA(isA<UserNotFoundError>()),
+        );
+      });
     });
   });
 
-  group('Communities get with auth', () {
+  group('Community get', () {
     setUp(() {
       setupSut();
-      setupAuthWithAUser();
     });
-
-    test('should read data from repo exactly only once -D', () async {
-      await sut.getComunitiesForUser();
-
-      verify(communityRepo.readCommunitiesByUser(any)).called(1);
-    });
-
-    test('should returns communities for currentUser -D.A', () async {
-      await sut.getComunitiesForUser();
-
-      verify(communityRepo.readCommunitiesByUser(aUserId));
-    });
-
-    test('should match total number communities with repos -D.B', () async {
-      setupCommunityRepoWithTwoCommunity();
-
-      final communities = await sut.getComunitiesForUser();
-
-      expect(communities.length, 2);
-    });
-
-    test('should returned community id matched with repo -D.C', () async {
-      setupCommunityRepoWithTwoCommunity();
-
-      final communities = await sut.getComunitiesForUser();
-
-      expect(communities.elementAt(0).id, aCommunityId);
-      expect(communities.elementAt(1).id, bCommunityId);
-    });
-    test('should returned community name matched with repo -D.D', () async {
-      setupCommunityRepoWithTwoCommunity();
-
-      final communities = await sut.getComunitiesForUser();
-
-      expect(communities.elementAt(0).name, aCommunityName);
-      expect(communities.elementAt(1).name, bCommunityName);
-    });
-    test('should returned community description matched with repo -D.E',
-        () async {
-      setupCommunityRepoWithTwoCommunity();
-
-      final communities = await sut.getComunitiesForUser();
-
-      expect(communities.elementAt(0).description, aCommunityDescription);
-      expect(communities.elementAt(1).description, bCommunityDescription);
-    });
-    test('should returned community profileImage matched with repo -D.F',
-        () async {
-      setupCommunityRepoWithTwoCommunity();
-
-      final communities = await sut.getComunitiesForUser();
-
-      expect(communities.elementAt(0).profileImage, aCommunityProfileImage);
-      expect(communities.elementAt(1).profileImage, bCommunityProfileImage);
-    });
-    test('should returned community coverImage matched with repo -D.G',
-        () async {
-      setupCommunityRepoWithTwoCommunity();
-
-      final communities = await sut.getComunitiesForUser();
-
-      expect(communities.elementAt(0).coverImage, aCommunityCoverImage);
-      expect(communities.elementAt(1).coverImage, bCommunityCoverImage);
-    });
-  });
-
-  group('Communities get without auth', () {
-    setUp(() {
-      setupSut();
-      setupAuthWithoutAUser();
-    });
-    test('should throw UserNotFoundError', () {
-      expect(() async {
+    group('with auth', () {
+      setUp(() {
+        setupAuthWithAUser();
+      });
+      test('should call repo.readCommunitiesByUser exactly only -D', () async {
         await sut.getComunitiesForUser();
-      }, throwsA(isA<UserNotFoundError>()));
+
+        verify(communityRepo.readCommunitiesByUser(any)).called(1);
+      });
+
+      test('should returns communities for user -D.A', () async {
+        await sut.getComunitiesForUser();
+
+        verify(communityRepo.readCommunitiesByUser(aUserId));
+      });
+
+      test('should get all communities for user -D.B', () async {
+        setupCommunityRepoWithTwoCommunity();
+
+        final communities = await sut.getComunitiesForUser();
+
+        expect(communities.length, 2);
+      });
+      group('shoud get property', () {
+        test('id -D.C', () async {
+          setupCommunityRepoWithTwoCommunity();
+
+          final communities = await sut.getComunitiesForUser();
+
+          expect(communities.elementAt(0).id, aCommunityId);
+          expect(communities.elementAt(1).id, bCommunityId);
+        });
+        test('name -D.D', () async {
+          setupCommunityRepoWithTwoCommunity();
+
+          final communities = await sut.getComunitiesForUser();
+
+          expect(communities.elementAt(0).name, aCommunityName);
+          expect(communities.elementAt(1).name, bCommunityName);
+        });
+        test('description -D.E', () async {
+          setupCommunityRepoWithTwoCommunity();
+
+          final communities = await sut.getComunitiesForUser();
+
+          expect(communities.elementAt(0).description, aCommunityDescription);
+          expect(communities.elementAt(1).description, bCommunityDescription);
+        });
+        test('profileImage -D.F', () async {
+          setupCommunityRepoWithTwoCommunity();
+
+          final communities = await sut.getComunitiesForUser();
+
+          expect(communities.elementAt(0).profileImage, aCommunityProfileImage);
+          expect(communities.elementAt(1).profileImage, bCommunityProfileImage);
+        });
+        test('coverImage -D.G', () async {
+          setupCommunityRepoWithTwoCommunity();
+
+          final communities = await sut.getComunitiesForUser();
+
+          expect(communities.elementAt(0).coverImage, aCommunityCoverImage);
+          expect(communities.elementAt(1).coverImage, bCommunityCoverImage);
+        });
+      });
+    });
+    group('without auth', () {
+      setUp(() {
+        setupAuthWithoutAUser();
+      });
+      test('should throw UserNotFoundError', () {
+        expect(() async {
+          await sut.getComunitiesForUser();
+        }, throwsA(isA<UserNotFoundError>()));
+      });
     });
   });
 
   group('Community archive', () {
+    setUp(() {
+      setupSut();
+    });
+
     group('with auth', () {
       setUp(() {
-        setupSut();
         setupAuthWithAUser();
       });
 
-      test('should call getCommunityMemberByUserId exactly once', () async {
+      test('should call repo.getCommunityMemberByUserId exactly once',
+          () async {
         try {
           await sut.archive(aCommunityId);
         } catch (e) {}
@@ -504,7 +499,6 @@ void main() {
     });
     group('without auth', () {
       setUp(() {
-        setupSut();
         setupAuthWithoutAUser();
       });
 
